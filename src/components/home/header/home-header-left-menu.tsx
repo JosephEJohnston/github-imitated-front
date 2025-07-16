583221059d392301d2aaef5a3e3000032fce8f50
JosephEJohnston <1193347519@qq.com>
2025-07-16 22:45:18 +0800

    feat: 顶部左侧栏，部分实现

--- src/components/home/header/home-header-left-menu.tsx
+++ src/components/home/header/home-header-left-menu.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+'use client'
+
 import $styles from './home-header-left-menu.module.css'
+import React, {useState} from 'react';
+import classNames from "classnames";
 
 const HomeHeaderLeftMenu = () => {
+    const [flag, setFlag] = useState(-1)
+
+    const menuShowClass =
+        flag === -1 ? $styles.menuHide : ``;
 
     return (
         <>
-            <div className={$styles.menu}>111</div>
+            <div className={classNames($styles.menu, menuShowClass)}>111</div>
             <div className="head-nav-common-button-div head-item-border">
-                <button>
+                <button onClick={() => setFlag(-flag)}>
                     <i className="bi bi-list"></i>
                 </button>
             </div>
