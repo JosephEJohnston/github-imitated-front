fdd0286494a1dcfa26cdcbee3655dc9b3fdd78d9
JosephEJohnston <1193347519@qq.com>
2025-07-18 23:15:29 +0800

    feat: 顶部左侧栏，menu，部分实现

--- src/components/home/header/home-header-left-menu.tsx
+++ src/components/home/header/home-header-left-menu.tsx
@@ -14,7 +14,53 @@ const HomeHeaderLeftMenu = (
             <div className={classNames($styles.menuShowContainer, menuShowClass)}>
                 <div className={$styles.menuContainer}>
                     <div className={$styles.menu}>
+                        <div>
+                            <span>icon</span>
+                            <button>x</button>
+                        </div>
+                        <div>
+                            <div>
+                                <span>icon</span>
+                                <span>Home</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Issues</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Pull requests</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Projects</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Discussions</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Codespaces</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Copilot</span>
+                            </div>
 
+                            <hr/>
+                            <div>
+                                <span>icon</span>
+                                <span>Explore</span>
+                            </div>
+                            <div>
+                                <span>icon</span>
+                                <span>Marketplace</span>
+                            </div>
+
+                            <hr/>
+
+                        </div>
                     </div>
                     <div onClick={showControlFunction} className={$styles.menuBackDrop}></div>
                 </div>
