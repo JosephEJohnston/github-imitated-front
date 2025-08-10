c65a94f1fe038797e37c36d0c6cb9f94ebd8db03
JosephEJohnston <1193347519@qq.com>
2025-08-10 22:31:28 +0800

    feat: 首页，顶部响应式布局，部分实现

--- src/components/home/header/header.tsx
+++ src/components/home/header/header.tsx
@@ -1,6 +1,9 @@
 import "./header.css"
+import "@/components/global-media.css"
+import $style from './header.module.css'
 import HomeHeaderLeftMenuButton from "@/components/home/header/home-header-left-menu-button";
 import TypeToSearch from "@/components/home/header/type-to-search";
+import classNames from "classnames";
 
 export default function Header() {
     return (
@@ -25,41 +28,43 @@ export default function Header() {
                     </div>
                     <div id="header-nav-right">
                         <TypeToSearch />
-                        <div id="combine-button" className="head-nav-common-button-div head-item-border">
-                            <button id="combine-button-left">
-                                <i className="bi bi-robot"></i>
-                            </button>
-                            <button id="combine-button-right">
-                                <i className="bi bi-chevron-down bi-icon-small"></i>
-                            </button>
-                        </div>
-                        <div className="head-nav-split">
-                            <div>|</div>
-                        </div>
-                        <div id="two-inner-button-div" className="head-nav-common-button-div head-item-border">
-                            <button id="two-inner-button">
+                        <div className={classNames($style.headerMiddleButtonList, 'block-more-than-small')}>
+                            <div id="combine-button" className="head-nav-common-button-div head-item-border">
+                                <button id="combine-button-left">
+                                    <i className="bi bi-robot"></i>
+                                </button>
+                                <button id="combine-button-right">
+                                    <i className="bi bi-chevron-down bi-icon-small"></i>
+                                </button>
+                            </div>
+                            <div className="head-nav-split">
+                                <div>|</div>
+                            </div>
+                            <div id="two-inner-button-div" className="head-nav-common-button-div head-item-border">
+                                <button id="two-inner-button">
                             <span className="button-inner-span">
                                 <i className="bi bi-plus bi-icon-middle"></i>
                             </span>
-                            <span className="button-inner-span">
+                                    <span className="button-inner-span">
                                 <i className="bi bi-chevron-down bi-icon-small"></i>
                             </span>
-                            </button>
-                        </div>
-                        <div className="head-nav-common-button-div head-item-border">
-                            <button>
-                                <i className="bi bi-record-circle bi-icon-middle"></i>
-                            </button>
-                        </div>
-                        <div className="head-nav-common-button-div head-item-border">
-                            <button>
-                                <i className="bi bi-bezier2 bi-icon-middle"></i>
-                            </button>
-                        </div>
-                        <div className="head-nav-common-button-div head-item-border">
-                            <button>
-                                <i className="bi bi-box-seam bi-icon-middle"></i>
-                            </button>
+                                </button>
+                            </div>
+                            <div className="head-nav-common-button-div head-item-border">
+                                <button>
+                                    <i className="bi bi-record-circle bi-icon-middle"></i>
+                                </button>
+                            </div>
+                            <div className="head-nav-common-button-div head-item-border">
+                                <button>
+                                    <i className="bi bi-bezier2 bi-icon-middle"></i>
+                                </button>
+                            </div>
+                            <div className="head-nav-common-button-div head-item-border">
+                                <button>
+                                    <i className="bi bi-box-seam bi-icon-middle"></i>
+                                </button>
+                            </div>
                         </div>
                         <div className="head-nav-common-button-div head-item-border">
                             <button>
