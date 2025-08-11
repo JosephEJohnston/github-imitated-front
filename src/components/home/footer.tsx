e9160acaa1fabfef03c606c47673667e1248d101
JosephEJohnston <1193347519@qq.com>
2025-08-11 22:07:09 +0800

    feat: 首页，底部响应式样式部分实现

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import "./footer.css"
+import $style from "./footer.module.css"
 
 const Footer = () => {
     return (
         <>
-            <footer id="content-footer" className="flex-space-between">
-                <button className="bottom-github"><i className="bi bi-github"></i></button>
-                <div><span>© 2025 GitHub, Inc.</span></div>
-                <div><a className="bottom-a" href="">Terms</a></div>
-                <div><a className="bottom-a" href="">Privacy</a></div>
-                <div><a className="bottom-a" href="">Security</a></div>
-                <div><a className="bottom-a" href="">Status</a></div>
-                <div><a className="bottom-a" href="">Docs</a></div>
-                <div><a className="bottom-a" href="">Contact</a></div>
-                <div><a className="bottom-a" href="">Manage cookies</a></div>
-                <div><a className="bottom-a" href="">Do not share my personal information</a></div>
+            <footer id="content-footer">
+                <div className={$style.footerDivLeft}>
+                    <button className="bottom-github"><i className="bi bi-github"></i></button>
+                    <div className={$style.footerLeftText}>© 2025 GitHub, Inc.</div>
+                </div>
+                <div className={$style.footerDivRight}>
+                    <div><a className="bottom-a" href="">Terms</a></div>
+                    <div><a className="bottom-a" href="">Privacy</a></div>
+                    <div><a className="bottom-a" href="">Security</a></div>
+                    <div><a className="bottom-a" href="">Status</a></div>
+                    <div><a className="bottom-a" href="">Docs</a></div>
+                    <div><a className="bottom-a" href="">Contact</a></div>
+                    <div><a className="bottom-a" href="">Manage cookies</a></div>
+                    <div><a className="bottom-a" href="">Do not share my personal information</a></div>
+                </div>
             </footer>
         </>
     );
