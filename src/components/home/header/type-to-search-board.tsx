aebb2c68489c60c09aecdbf9f367e1b4b9ccaee9
JosephEJohnston <1193347519@qq.com>
2025-08-13 21:51:50 +0800

    feat: 首页，顶部搜索表单，初始化

---
+++ src/components/home/header/type-to-search-board.tsx
@@ -0,0 +1,28 @@
+'use client'
+import $style from '@/components/home/header/type-to-search-board.module.css'
+import React from 'react';
+
+interface Props {
+    showBoard: boolean;
+    setShowBoard: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const TypeToSearchBoard = (
+    {
+        showBoard,
+        setShowBoard
+    }: Props
+) => {
+
+    const showControl = showBoard ? $style.showBoard : $style.hideBoard;
+
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    );
+};
+
+export default TypeToSearchBoard;
--- src/components/home/header/type-to-search.tsx
+++ src/components/home/header/type-to-search.tsx
@@ -1,13 +1,17 @@
+'use client'
 import "@/components/global-media.css";
-import React from 'react';
+import React, {useState} from 'react';
 import $style from '@/components/home/header/type-to-search.module.css'
 import classNames from "classnames";
+import TypeToSearchBoard from "@/components/home/header/type-to-search-board";
 
 const TypeToSearch = () => {
+    const [showBoard, setShowBoard] = useState(false);
+
     return (
         <>
             <div id="type-to-search-div" className={classNames('head-item-border', 'block-more-than-big')}>
-                <button className={$style.typeToSearchButton}>
+                <button className={$style.typeToSearchButton} onClick={() => setShowBoard(true)}>
                     <span>
                         <i className="bi bi-search"></i>
                     </span>
@@ -20,13 +24,13 @@ const TypeToSearch = () => {
                 </button>
             </div>
             <div className={classNames('head-item-border', 'block-less-than-big')}>
-                <button className={$style.typeToSearchButton}>
+                <button className={$style.typeToSearchButton} onClick={() => setShowBoard(true)}>
                     <span>
                         <i className="bi bi-search"></i>
                     </span>
                 </button>
             </div>
-
+            <TypeToSearchBoard showBoard={showBoard} setShowBoard={setShowBoard} />
         </>
     );
 };
