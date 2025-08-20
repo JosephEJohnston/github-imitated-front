9a7786c8e821e16a098b2c816451c1db334773e6
JosephEJohnston <1193347519@qq.com>
2025-08-20 22:49:05 +0800

    feat: 首页，蒙层组件，传入子宽度，初始化

--- src/components/common/mask-layer.tsx
+++ src/components/common/mask-layer.tsx
@@ -2,30 +2,38 @@
 import React from 'react';
 import classNames from "classnames";
 import $styles from './mask-layer.module.css';
+import styled from "styled-components";
 
 interface Props {
     showFlag: number;
     showControlFunction : () => void;
     children: React.ReactNode;
+    childrenWidthRem?: number;
 }
 
 const MaskLayer = (
     {
         showFlag,
         showControlFunction,
-        children
+        children,
+        childrenWidthRem
     } : Props
 ) => {
 
     const menuShowClass =
         showFlag === -1 ? $styles.childrenHide : $styles.childrenShow;
 
+    const ChildrenBackDrop = styled.div`
+        height: 100vh;
+        width: calc(100vw - ${childrenWidthRem || 20}rem);
+    ;`
+
     return (
         <>
             <div className={classNames($styles.maskLayerContainer, menuShowClass)}>
                 <div className={$styles.maskLayer}>
                     { children }
-                    <div onClick={showControlFunction} className={$styles.childrenBackDrop}></div>
+                    <ChildrenBackDrop onClick={showControlFunction}></ChildrenBackDrop>
                 </div>
             </div>
         </>
