15331a9b5cd03f866eb9b5f2d5f8425fd770428a
JosephEJohnston <1193347519@qq.com>
2025-09-15 22:22:17 +0800

    feat: 首页，顶部右侧按钮，copilot menu，部分实现

--- src/components/home/header/copilot-button.tsx
+++ src/components/home/header/copilot-button.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import "./header.css"
 import classNames from "classnames";
-import $style from "@/components/home/header/header.module.css";
+import $headerStyle from "@/components/home/header/header.module.css";
+import $style from "@/components/home/header/copilot-button.module.css";
 
 const CopilotButton = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <>
-            <div id="combine-button" className="head-nav-common-button-div head-item-border">
-                <button id="combine-button-left"
-                        className={classNames($style.withHoverText)}
+            <div className={classNames(
+                     $style.combineButton,
+                     'head-nav-common-button-div',
+                     'head-item-border'
+                 )}
+            >
+                <button className={classNames(
+                            $style.combineButtonLeft,
+                            $headerStyle.withHoverText
+                        )}
                         data-tooltip={"Chat with Copilot"}
                 >
                     <i className="bi bi-robot"></i>
                 </button>
-                <button id="combine-button-right"
-                        className={classNames($style.withHoverText)}
+                <button className={classNames(
+                            $style.combineButtonRight,
+                            $headerStyle.withHoverText
+                        )}
                         data-tooltip={"Open Copilot..."}
+                        onClick={() => setShowMenu(!showMenu)}
                 >
                     <i className="bi bi-chevron-down bi-icon-small"></i>
                 </button>
+                {showMenu && (
+                    <div className={$style.copilotMenu}>
+                        hello world
+                    </div>
+                )}
             </div>
         </>
     );
