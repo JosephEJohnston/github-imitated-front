5d8dcce4480b0f14dd5ff525bbcbef6b17e8c47b
JosephEJohnston <1193347519@qq.com>
2025-09-27 22:21:27 +0800

    feat: 首页，顶部右侧按钮，create new menu，部分实现

--- src/components/home/header/create-new-button.tsx
+++ src/components/home/header/create-new-button.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+'use client'
+import React, {RefObject, useRef, useState} from 'react';
 import classNames from "classnames";
 import $headerStyle from "@/components/home/header/header.module.css";
 import $style from "@/components/home/header/create-new-button.module.css";
 
 
 const CreateNewButton = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+    const menuRef: RefObject<HTMLDivElement | null> = useRef(null);
+
     return (
         <>
             <div id="two-inner-button-div"
@@ -16,7 +21,7 @@ const CreateNewButton = () => {
                  )}
                  data-tooltip={"Create new..."}
                 >
-                <button id="two-inner-button">
+                <button id="two-inner-button" onClick={() => setShowMenu(!showMenu)}>
                     <span className="button-inner-span">
                         <i className="bi bi-plus bi-icon-middle"></i>
                     </span>
@@ -24,17 +29,30 @@ const CreateNewButton = () => {
                         <i className="bi bi-chevron-down bi-icon-small"></i>
                     </span>
                 </button>
+                <CreateNewMenu ref={menuRef} showControl={showMenu} />
             </div>
         </>
     );
 };
 
-const CreateNewMenu = () => {
+interface CreateNewMenuProps {
+    ref: RefObject<HTMLDivElement | null>;
+    showControl: boolean;
+}
+
+const CreateNewMenu = (
+    props: CreateNewMenuProps
+) => {
+
+    const showClass = props.showControl ? $style.createNewMenuShow :
+        $style.createNewMenuHide;
 
     return (
         <>
-            <div>
-
+            <div className={classNames($style.createNewMenu, showClass)} ref={props.ref}>
+                <div className={$style.createNewMenuRow}>
+                    Hello world.
+                </div>
             </div>
         </>
     )
